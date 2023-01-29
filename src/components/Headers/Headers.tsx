2986d820fc81d73810e1602a7159d7036d9ec784
pardorn3237 <paradorn3237@gmail.com>
2023-01-29 15:19:27 +0700

    .

--- src/components/Headers/Headers.tsx
+++ src/components/Headers/Headers.tsx
@@ -5,16 +5,16 @@ import { appWindow } from '@tauri-apps/api/window'
 const Headers: Component = () => {
     return (
     <div data-tauri-drag-region class="bar">
-        <div class="titlebar-button close" id="titlebar-close" onClick={()=> appWindow.close()}>
+        <div class="titlebar-button flex" id="titlebar-close" onClick={()=> appWindow.close()}>
             <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
         </div>
-        <div class="titlebar-button change" id="titlebar-maximize" onClick={()=> appWindow.toggleMaximize()}>
+        <div class="titlebar-button flex" id="titlebar-maximize" onClick={()=> appWindow.toggleMaximize()}>
             <img
             src="https://api.iconify.design/mdi:window-maximize.svg"
             alt="maximize"
             />
         </div>
-        <div class="titlebar-button kill" id="titlebar-minimize" onClick={()=> appWindow.minimize()}>
+        <div class="titlebar-button flex " id="titlebar-minimize" onClick={()=> appWindow.minimize()}>
             <img
             src="https://api.iconify.design/mdi:window-minimize.svg"
             alt="minimize"
